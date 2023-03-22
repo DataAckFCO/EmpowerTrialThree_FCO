7eae5496c196be0f24dd2a8f8de625aef3eacc24
Floris Cockaerts <floris.cockaerts@skyline.be>
2023-03-22 14:14:43 +0100

    Add project files.

---
+++ EmpowerTrialThree_1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerTrialThree_1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerTrialThree_1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0864D7DE-8DCB-4361-BC72-DF80B803F947")]
+[assembly: AssemblyVersion("1.0.0.0")]
